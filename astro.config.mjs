c49396be963ca3dab1c9d053f3846e042de9eca6
rezamaulana13 <rezamaulanaply@gmail.com>
2026-07-12 21:37:32 +0700

    fix: use compatible vercel adapter version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    edgeMiddleware: false,
+  }),
   integrations: [react(), tailwind()],
 });
