c628d0ce62b640c2d0c6cb5979ad16741d68793b
rezamaulana13 <rezamaulanaply@gmail.com>
2026-07-12 20:06:32 +0700

    literly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://waruturi-2027.com',
+  output: 'server',
+  adapter: netlify(),
   output: 'server', // <- Ini cukup, adapter tidak perlu
 });
