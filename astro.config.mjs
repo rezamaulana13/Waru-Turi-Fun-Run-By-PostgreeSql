2d5662580236e2305f169b65a4428d41f18d8e24
rezamaulana13 <rezamaulanaply@gmail.com>
2026-07-12 21:32:48 +0700

    fix: use edge runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/edge';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    runtime: 'nodejs20.x',  // atau 'nodejs18.x'
-  }),
+  adapter: vercel(),
   integrations: [react(), tailwind()],
 });
